35db644c4e5306365e1cef44dbe55bd174ea7a11
vmakarov <vmakarov>
2007-02-03 00:11:00 +0000

    2007-02-02  Vladimir Makarov  <vmakarov@redhat.com>
    	* DINO/dino.tst.in: New test for gmp.
    	* DINO/gmp.c: Add stdlib.h
    	(mpz_binary_op): Rename to mpz_three_op.
    	(mpz_two_op_ui, mpz_two_op, mpz_one_op_ui, return_float,
    	arithm_unary_op, op_ui, int_op_ui, _z_sub, _z_neg, _z_abs,
    	_z_tdiv_r, _z_pow_ui, _z_root, int_res_op, _z_cmp, _z_ior, _z_xor,
    	_z_and, _z_com, _z_set, _z_set_str, _z_get_d, _z_get_str,
    	_z_set_bit, _z_clr_bit, _z_tst_bit, _z_urandomm): New functions.
    	(mpz_address): Add the new _z functions.
    	* DINO/gmp.d (_z_sub, _z_neg, _z_abs, _z_tdiv_r, _z_pow_ui,
    	_z_root, _z_cmp, _z_ior, _z_xor, _z_and, _z_com, _z_set,
    	_z_set_str, _z_get_d, _z_get_str, _z_set_bit, _z_clr_bit,
    	_z_tst_bit, _z_urandomm): New external declarations.
    	(check_ui, set, set_str, get_f, get_str, set_bit, clr_bit,
    	tst_bit, sub, neg, abs, mul, mod, pow_ui, root, cmp, ior, xor,
    	and, com, rand): New functions.
    	(tdiv_q): Rename to div.

--- DINO/gmp.c
+++ DINO/gmp.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 1997-2005 Vladimir Makarov.
+   Copyright (C) 1997-2007 Vladimir Makarov.
 
    Written by Vladimir Makarov <vmakarov@users.sourceforge.net>
 
@@ -26,6 +26,7 @@
    operand types. */
 
 #include "d_extern.h"
+#include <stdlib.h>
 
 #ifdef HAVE_GMP_H
 #include "gmp.h"
@@ -78,14 +79,12 @@ _z_clear (int npars, val_t *vals)
 }
 
 static void
-mpz_binary_op (int npars, val_t *vals, void **hidevalue1, void **hidevalue2,
-	       void **res_hidevalue)
+mpz_three_op (int npars, val_t *vals, void **hidevalue1, void **hidevalue2,
+	       void **first_hidevalue)
 {
   ER_node_t var1;
   ER_node_t var2;
-  ER_node_t res_var;
-  ER_node_t init_var;
-  ER_node_t hideblock;
+  ER_node_t first_var;
 
   assert (npars == 3
 	  && ER_NODE_MODE ((ER_node_t) vals) == ER_NM_instance
@@ -95,10 +94,62 @@ mpz_binary_op (int npars, val_t *vals, void **hidevalue1, void **hidevalue2,
 		      1);
   var2 = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) (vals + 2))),
 		      1);
-  res_var = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) vals)), 1);
+  first_var
+    = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) vals)), 1);
   *hidevalue1 = ER_hideblock_start (ER_hideblock (var1));
   *hidevalue2 = ER_hideblock_start (ER_hideblock (var2));
-  *res_hidevalue = ER_hideblock_start (ER_hideblock (res_var));
+  *first_hidevalue = ER_hideblock_start (ER_hideblock (first_var));
+}
+
+static void
+mpz_two_op_ui (int npars, val_t *vals, void **hidevalue, int_t *ui,
+	       void **first_hidevalue)
+{
+  ER_node_t var1;
+  ER_node_t first_var;
+
+  assert (npars == 3
+	  && ER_NODE_MODE ((ER_node_t) vals) == ER_NM_instance
+	  && ER_NODE_MODE ((ER_node_t) (vals + 1)) == ER_NM_instance
+	  && ER_NODE_MODE ((ER_node_t) (vals + 2)) == ER_NM_int);
+  var1 = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) (vals + 1))),
+		      1);
+  first_var
+    = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) vals)), 1);
+  *hidevalue = ER_hideblock_start (ER_hideblock (var1));
+  *ui = ER_i ((ER_node_t) (vals + 2));
+  *first_hidevalue = ER_hideblock_start (ER_hideblock (first_var));
+}
+
+static void
+mpz_two_op (int npars, val_t *vals, void **hidevalue, void **first_hidevalue)
+{
+  ER_node_t var;
+  ER_node_t first_var;
+
+  assert (npars == 2
+	  && ER_NODE_MODE ((ER_node_t) vals) == ER_NM_instance
+	  && ER_NODE_MODE ((ER_node_t) (vals + 1)) == ER_NM_instance);
+  var = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) (vals + 1))),
+		     1);
+  first_var
+    = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) vals)), 1);
+  *hidevalue = ER_hideblock_start (ER_hideblock (var));
+  *first_hidevalue = ER_hideblock_start (ER_hideblock (first_var));
+}
+
+static void
+mpz_one_op_ui (int npars, val_t *vals, int_t *ui, void **first_hidevalue)
+{
+  ER_node_t first_var;
+
+  assert (npars == 2
+	  && ER_NODE_MODE ((ER_node_t) vals) == ER_NM_instance
+	  && ER_NODE_MODE ((ER_node_t) (vals + 1)) == ER_NM_int);
+  first_var
+    = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) vals)), 1);
+  *ui = ER_i ((ER_node_t) (vals + 1));
+  *first_hidevalue = ER_hideblock_start (ER_hideblock (first_var));
 }
 
 static val_t
@@ -113,13 +164,59 @@ return_int (int_t i)
 }
 
 static val_t
-arithm_op (int npars, val_t *vals, void (*func) (mpz_t, const mpz_t, const mpz_t))
+return_float (floating_t f)
+{
+  val_t val;
+  ER_node_t res = (ER_node_t) &val;
+
+  ER_SET_MODE (res, ER_NM_float);
+  ER_set_f (res, f);
+  return val;
+}
+
+static val_t
+arithm_op (int npars, val_t *vals,
+	   void (*func) (mpz_t, const mpz_t, const mpz_t))
 {
   void *hidevalue1, *hidevalue2, *res_hidevalue;
 
-  mpz_binary_op (npars, vals, &hidevalue1, &hidevalue2, &res_hidevalue);
+  mpz_three_op (npars, vals, &hidevalue1, &hidevalue2, &res_hidevalue);
   (*func) (*(mpz_t *) res_hidevalue, *(mpz_t *) hidevalue1,
 	   *(mpz_t *) hidevalue2);
+  return vals [0];
+}
+
+static val_t
+arithm_unary_op (int npars, val_t *vals, void (*func) (mpz_t, const mpz_t))
+{
+  void *hidevalue, *res_hidevalue;
+
+  mpz_two_op (npars, vals, &hidevalue, &res_hidevalue);
+  (*func) (*(mpz_t *) res_hidevalue, *(mpz_t *) hidevalue);
+  return vals [0];
+}
+
+static val_t
+op_ui (int npars, val_t *vals,
+       void (*func) (mpz_t, const mpz_t, unsigned long ui))
+{
+  void *hidevalue, *res_hidevalue;
+  int_t ui;
+
+  mpz_two_op_ui (npars, vals, &hidevalue, &ui, &res_hidevalue);
+  (*func) (*(mpz_t *) res_hidevalue, *(mpz_t *) hidevalue, (unsigned long) ui);
+  return vals [0];
+}
+
+static val_t
+int_op_ui (int npars, val_t *vals,
+	   int (*func) (mpz_t, const mpz_t, unsigned long ui))
+{
+  void *hidevalue, *res_hidevalue;
+  int_t ui;
+
+  mpz_two_op_ui (npars, vals, &hidevalue, &ui, &res_hidevalue);
+  (*func) (*(mpz_t *) res_hidevalue, *(mpz_t *) hidevalue, (unsigned long) ui);
   return vals [0];
 }
 
@@ -127,6 +224,24 @@ val_t
 _z_add (int npars, val_t *vals)
 {
   return arithm_op (npars, vals, mpz_add);
+}
+
+val_t
+_z_sub (int npars, val_t *vals)
+{
+  return arithm_op (npars, vals, mpz_sub);
+}
+
+val_t
+_z_neg (int npars, val_t *vals)
+{
+  return arithm_unary_op (npars, vals, mpz_neg);
+}
+
+val_t
+_z_abs (int npars, val_t *vals)
+{
+  return arithm_unary_op (npars, vals, mpz_abs);
 }
 
 val_t
@@ -142,6 +257,91 @@ _z_tdiv_q (int npars, val_t *vals)
 }
 
 val_t
+_z_tdiv_r (int npars, val_t *vals)
+{
+  return arithm_op (npars, vals, mpz_tdiv_r);
+}
+
+val_t
+_z_pow_ui (int npars, val_t *vals)
+{
+  return op_ui (npars, vals, mpz_pow_ui);
+}
+
+val_t
+_z_root (int npars, val_t *vals)
+{
+  return int_op_ui (npars, vals, mpz_root);
+}
+
+static val_t
+int_res_op (int npars, val_t *vals, int (*func) (const mpz_t, const mpz_t))
+{
+  void *hidevalue1, *hidevalue2;
+  int res;
+
+  mpz_two_op (npars, vals, &hidevalue2, &hidevalue1);
+  res = (*func) (*(mpz_t *) hidevalue1, *(mpz_t *) hidevalue2);
+  return return_int (res);
+}
+
+val_t
+_z_cmp (int npars, val_t *vals)
+{
+  return int_res_op (npars, vals, mpz_cmp);
+}
+
+val_t
+_z_ior (int npars, val_t *vals)
+{
+  return arithm_op (npars, vals, mpz_ior);
+}
+
+val_t
+_z_xor (int npars, val_t *vals)
+{
+  return arithm_op (npars, vals, mpz_xor);
+}
+
+val_t
+_z_and (int npars, val_t *vals)
+{
+  return arithm_op (npars, vals, mpz_and);
+}
+
+val_t
+_z_com (int npars, val_t *vals)
+{
+  return arithm_unary_op (npars, vals, mpz_com);
+}
+
+val_t
+_z_set (int npars, val_t *vals)
+{
+  void *hidevalue, *res_hidevalue;
+
+  mpz_two_op (npars, vals, &hidevalue, &res_hidevalue);
+  mpz_set (*(mpz_t *) res_hidevalue, *(mpz_t *) hidevalue);
+  return return_nil ();
+}
+
+val_t
+_z_set_str (int npars, val_t *vals)
+{
+  ER_node_t res_var, var;
+
+  assert (npars == 2
+	  && ER_NODE_MODE ((ER_node_t) vals) == ER_NM_instance
+	  && ER_NODE_MODE ((ER_node_t) (vals + 1)) == ER_NM_vect);
+  var = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) (vals + 1))),
+		     1);
+  res_var = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) vals)), 1);
+  mpz_set_str (*(mpz_t *) ER_hideblock_start (ER_hideblock (res_var)),
+	       ER_pack_els (ER_vect ((ER_node_t) (vals + 1))), 0);
+  return return_nil ();
+}
+
+val_t
 _z_get_si (int npars, val_t *vals)
 {
   ER_node_t var;
@@ -150,6 +350,97 @@ _z_get_si (int npars, val_t *vals)
   var = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) vals)), 1);
   return return_int (mpz_get_si (*(mpz_t *)
 				 ER_hideblock_start (ER_hideblock (var))));
+}
+
+val_t
+_z_get_d (int npars, val_t *vals)
+{
+  ER_node_t var;
+
+  assert (npars == 1 && ER_NODE_MODE ((ER_node_t) vals) == ER_NM_instance);
+  var = INDEXED_VAL (ER_instance_vars (ER_instance ((ER_node_t) vals)), 1);
+  return return_float (mpz_get_d (*(mpz_t *)
+				  ER_hideblock_start (ER_hideblock (var))));
+}
+
+val_t
+_z_get_str (int npars, val_t *vals)
+{
+  val_t val;
+  ER_node_t res = (ER_node_t) &val;
+  ER_node_t vect;
+  void *hidevalue;
+  char *str, *start;
+  int_t ui;
+  int len;
+
+  mpz_one_op_ui (npars, vals, &ui, &hidevalue);
+  len = mpz_sizeinbase (*(mpz_t *) hidevalue, ui) + 4;
+  start = str = malloc (len);
+  if (ui == 16)
+    {
+      *str++= '0';
+      *str++= 'X';
+    }
+  else if (ui == 2)
+    {
+      *str++= '0';
+      *str++= 'B';
+    }
+  else if (ui == 8)
+    *str++= '0';
+  mpz_get_str (str, ui, *(mpz_t *) hidevalue);
+  vect = create_string (start);
+  free (start);
+  ER_SET_MODE (res, ER_NM_vect);
+  ER_set_vect (res, vect);
+  return val;
+}
+
+val_t
+_z_set_bit (int npars, val_t *vals)
+{
+  void *res_hidevalue;
+  int_t ui;
+
+  mpz_one_op_ui (npars, vals, &ui, &res_hidevalue);
+  mpz_setbit (*(mpz_t *) res_hidevalue, ui);
+  return return_nil ();
+}
+
+val_t
+_z_clr_bit (int npars, val_t *vals)
+{
+  void *res_hidevalue;
+  int_t ui;
+
+  mpz_one_op_ui (npars, vals, &ui, &res_hidevalue);
+  mpz_clrbit (*(mpz_t *) res_hidevalue, ui);
+  return return_nil ();
+}
+
+val_t
+_z_tst_bit (int npars, val_t *vals)
+{
+  void *res_hidevalue;
+  int_t ui;
+
+  mpz_one_op_ui (npars, vals, &ui, &res_hidevalue);
+  return return_int (mpz_tstbit (*(mpz_t *) res_hidevalue, ui));
+}
+
+val_t
+_z_urandomm (int npars, val_t *vals)
+{
+  void *hidevalue, *res_hidevalue;
+  int_t ui;
+  static gmp_randstate_t state;
+
+  mpz_two_op_ui (npars, vals, &hidevalue, &ui, &res_hidevalue);
+  if (ui != 0)
+    gmp_randinit_default (state);
+  mpz_urandomm (*(mpz_t *) res_hidevalue, state, *(mpz_t *) hidevalue);
+  return vals [0];
 }
 
 #endif /* #ifdef HAVE_GMP_H */
@@ -161,19 +452,59 @@ _z_get_si (int npars, val_t *vals)
 void *
 mpz_address (const char *name)
 {
+#ifdef HAVE_GMP_H
   if (strcmp (name, "_z_create") == 0)
     return _z_create;
   else if (strcmp (name, "_z_clear") == 0)
     return _z_clear;
+  else if (strcmp (name, "_z_set") == 0)
+    return _z_set;
+  else if (strcmp (name, "_z_set_str") == 0)
+    return _z_set_str;
   else if (strcmp (name, "_z_get_si") == 0)
     return _z_get_si;
+  else if (strcmp (name, "_z_get_d") == 0)
+    return _z_get_d;
+  else if (strcmp (name, "_z_get_str") == 0)
+    return _z_get_str;
+  else if (strcmp (name, "_z_set_bit") == 0)
+    return _z_set_bit;
+  else if (strcmp (name, "_z_clr_bit") == 0)
+    return _z_clr_bit;
+  else if (strcmp (name, "_z_tst_bit") == 0)
+    return _z_tst_bit;
   else if (strcmp (name, "_z_add") == 0)
     return _z_add;
+  else if (strcmp (name, "_z_sub") == 0)
+    return _z_sub;
+  else if (strcmp (name, "_z_neg") == 0)
+    return _z_neg;
+  else if (strcmp (name, "_z_abs") == 0)
+    return _z_abs;
   else if (strcmp (name, "_z_mul") == 0)
     return _z_mul;
   else if (strcmp (name, "_z_tdiv_q") == 0)
     return _z_tdiv_q;
+  else if (strcmp (name, "_z_tdiv_r") == 0)
+    return _z_tdiv_r;
+  else if (strcmp (name, "_z_pow_ui") == 0)
+    return _z_pow_ui;
+  else if (strcmp (name, "_z_root") == 0)
+    return _z_root;
+  else if (strcmp (name, "_z_cmp") == 0)
+    return _z_cmp;
+  else if (strcmp (name, "_z_ior") == 0)
+    return _z_ior;
+  else if (strcmp (name, "_z_xor") == 0)
+    return _z_xor;
+  else if (strcmp (name, "_z_and") == 0)
+    return _z_and;
+  else if (strcmp (name, "_z_com") == 0)
+    return _z_com;
+  else if (strcmp (name, "_z_urandomm") == 0)
+    return _z_com;
   else
+#endif /* #ifdef HAVE_GMP_H */
     return NULL;
 }
 #endif
--- DINO/gmp.d
+++ DINO/gmp.d
@@ -33,15 +33,25 @@ ext except {
 final class mpz_package () {
   var gmp_excepts = excepts.gmp_except ();
 
-  extern _z_create(), _z_clear(), _z_get_si(), _z_add(), _z_mul(), _z_tdiv_q();
-  private _z_create, _z_clear, _z_get_si, _z_add, _z_mul, _z_tdiv_q;
-  private check, check2;
+  extern _z_create(), _z_clear(), _z_set(), _z_set_str(), _z_get_si(),
+         _z_get_d(), _z_get_str(), _z_set_bit(), _z_clr_bit(), _z_tst_bit(),
+         _z_add(), _z_sub(), _z_neg(), _z_abs(), _z_mul(), _z_tdiv_q(),
+         _z_tdiv_r(), _z_pow_ui(), _z_root(), _z_cmp(), _z_ior(), _z_xor(),
+         _z_and(), _z_com(), _z_urandomm();
+  private _z_create, _z_clear, _z_set, _z_set_str, _z_get_si, _z_get_d,
+          _z_get_str, _z_set_bit, _z_clr_bit, _z_tst_bit, _z_add, _z_sub,
+          _z_neg, _z_abs, _z_mul, _z_tdiv_q, _z_tdiv_r, _z_cmp, _z_pow_ui,
+          _z_root, _z_ior, _z_xor, _z_and, _z_com, _z_urandomm;
+  private check, check2, check_ui;
 
   final class mpz (i) {
     private mpz_val; var mpz_val;
-    if (type (i) != int) gmp_excepts.gmp_param_val ();
+    if (type (i) != int) throw gmp_excepts.gmp_param_val ();
     mpz_val = _z_create (i);
     func destroy () { _z_clear (mpz_val); }
+  }
+  func check_ui (n) {
+    if (type (n) != int || n < 0) throw gmp_excepts.gmp_param_val ();
   }
   func check (op) {
     if (type (op) != class () || ! inside (op, mpz_package))
@@ -52,10 +62,48 @@ final class mpz_package () {
 	|| type (op2) != class () || ! inside (op2, mpz_package))
       throw gmp_excepts.gmp_type ();
   }
+  func set (lhs, rhs) {check2 (lhs, rhs);  return _z_set (lhs, rhs);}
+  func set_str (lhs, str) {
+    check (lhs);
+    if (type (str) != vector ||  eltype (str) != char)
+      throw gmp_excepts.gmp_type();
+    return _z_set_str (lhs, str);
+  }
   func get_i (op)  { check (op); return _z_get_si (op); }
+  func get_f (op)  { check (op); return _z_get_d (op); }
+  func get_str (op, base)  {
+    check (op);
+    if (type (base) != int
+	|| (base != 10 && base != 2 && base != 8 && base != 16))
+      throw gmp_excepts.gmp_param_val ();
+    return _z_get_str (op, base);
+  }
+  func set_bit (op, n) { check (op); check_ui (n); return _z_set_bit (op, n); }
+  func clr_bit (op, n) { check (op); check_ui (n); return _z_clr_bit (op, n); }
+  func tst_bit (op, n) { check (op); check_ui (n); return _z_tst_bit (op, n); }
   func add (op1, op2) { check2 (op1, op2); return _z_add (mpz (0), op1, op2); }
+  func sub (op1, op2) { check2 (op1, op2); return _z_sub (mpz (0), op1, op2); }
+  func neg (op) { check (op); return _z_neg (mpz (0), op); }
+  func abs (op) { check (op); return _z_abs (mpz (0), op); }
   func mul (op1, op2) { check2 (op1, op2); return _z_mul (mpz (0), op1, op2); }
-  func tdiv_q (n, d) { check2 (n, d); return _z_tdiv_q (mpz (0), n, d); }
+  func div (n, d) { check2 (n, d); return _z_tdiv_q (mpz (0), n, d); }
+  func mod (n, d) { check2 (n, d); return _z_tdiv_r (mpz (0), n, d); }
+  func pow_ui (base, n) {
+    check (base); check_ui (n); return _z_pow_ui (mpz (0), base, n);
+  }
+  func root (op, n) {
+    check (op); check_ui (n); return _z_root (mpz (0), op, n);
+  }
+  func cmp (op1, op2) { check2 (op1, op2); return _z_cmp (op1, op2); }
+  func ior (op1, op2) { check2 (op1, op2); return _z_ior (mpz (0), op1, op2); }
+  func xor (op1, op2) { check2 (op1, op2); return _z_xor (mpz (0), op1, op2); }
+  func and (op1, op2) { check2 (op1, op2); return _z_and (mpz (0), op1, op2); }
+  func com (op) { check (op); return _z_com (mpz (0), op); }
+  func rand (n, first) {
+    if (type (first) != int) throw gmp_excepts.gmp_param_val ();
+    check (n);
+    return _z_urandomm (mpz (0), n, first);
+  }
 }
 
 var mpzs = mpz_package ();
